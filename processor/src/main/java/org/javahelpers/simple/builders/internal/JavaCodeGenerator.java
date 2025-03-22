988db0d605c3e3876f18e0ae693248b3f17eff03
AndreasIgel <AndreasIgel82@gmail.com>
2025-03-22 20:40:27 +0100

    Adding MIT license information

--- processor/src/main/java/org/javahelpers/simple/builders/internal/JavaCodeGenerator.java
+++ processor/src/main/java/org/javahelpers/simple/builders/internal/JavaCodeGenerator.java
@@ -1,3 +1,27 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2025 Andreas Igel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package org.javahelpers.simple.builders.internal;
 
 import static javax.lang.model.element.Modifier.PUBLIC;
@@ -44,8 +68,9 @@ public class JavaCodeGenerator {
     classBuilder.addField(
         FieldSpec.builder(buildingTargetClassName, "instance", Modifier.PRIVATE, Modifier.FINAL)
             .build());
-    
-    //TODO: Constructors sollten in der BuilderDefinitionDto definiert werden, nur für Klassen mit leerem Constructor ist auch ein Builder mit leerem Constructor möglich
+
+    // TODO: Constructors sollten in der BuilderDefinitionDto definiert werden, nur für Klassen mit
+    // leerem Constructor ist auch ein Builder mit leerem Constructor möglich
     classBuilder.addMethod(generateConstructor(buildingTargetClassName));
     classBuilder.addMethod(generateEmptyConstructor(buildingTargetClassName));
 
