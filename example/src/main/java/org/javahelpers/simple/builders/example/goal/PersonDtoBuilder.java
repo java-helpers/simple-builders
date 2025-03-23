3d778ec234cbe63ad2babc091493c24d4ac95adf
AndreasIgel <AndreasIgel82@gmail.com>
2025-03-23 22:17:22 +0100

    Improving build and renaming generator to processor

--- example/src/main/java/org/javahelpers/simple/builders/example/goal/PersonDtoBuilder.java
+++ example/src/main/java/org/javahelpers/simple/builders/example/goal/PersonDtoBuilder.java
@@ -1,18 +1,18 @@
 /*
  * MIT License
- * 
+ *
  * Copyright (c) 2025 Andreas Igel
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -31,36 +31,38 @@ import org.javahelpers.simple.builders.example.MannschaftDto;
 import org.javahelpers.simple.builders.example.PersonDto;
 
 @Generated("org.javahelpers.simple.builders.processor")
-@BuilderImplementation(forClass=PersonDto.class)
+@BuilderImplementation(forClass = PersonDto.class)
 public class PersonDtoBuilder {
   private final PersonDto instance = new PersonDto();
 
-  public static PersonDtoBuilder create(){
+  public static PersonDtoBuilder create() {
     return new PersonDtoBuilder();
   }
 
-  public PersonDtoBuilder name(String name){
+  public PersonDtoBuilder name(String name) {
     instance.setName(name);
     return this;
   }
 
-  public PersonDtoBuilder birthdate(java.time.LocalDate birthdate){
+  public PersonDtoBuilder birthdate(java.time.LocalDate birthdate) {
     instance.setBirthdate(birthdate);
     return this;
   }
 
-  public PersonDtoBuilder mannschaft(MannschaftDto mannschaft){
+  public PersonDtoBuilder mannschaft(MannschaftDto mannschaft) {
     instance.setMannschaft(mannschaft);
     return this;
   }
-  public PersonDtoBuilder mannschaft(Consumer<org.javahelpers.simple.builders.example.goal.MannschaftDtoBuilder> lamda){
+
+  public PersonDtoBuilder mannschaft(
+      Consumer<org.javahelpers.simple.builders.example.goal.MannschaftDtoBuilder> lamda) {
     MannschaftDtoBuilder builder = MannschaftDtoBuilder.create();
     lamda.accept(builder);
     instance.setMannschaft(builder.build());
     return this;
   }
 
-  public PersonDto build(){
+  public PersonDto build() {
     return instance;
   }
 }
--- example/src/test/java/org/javahelpers/simple/builders/example/PersonDtoBuilderTest.java
+++ example/src/test/java/org/javahelpers/simple/builders/example/PersonDtoBuilderTest.java
@@ -1,18 +1,18 @@
 /*
  * MIT License
- * 
+ *
  * Copyright (c) 2025 Andreas Igel
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,33 +24,27 @@
 
 package org.javahelpers.simple.builders.example;
 
-import java.time.LocalDate;
-import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.time.LocalDate;
 import org.junit.jupiter.api.Test;
 
 public class PersonDtoBuilderTest {
 
   @Test
   public void testBuilder() {
-    PersonDto personDto = PersonDtoBuilder
-            .create()
+    PersonDto personDto =
+        PersonDtoBuilder.create()
             .birthdate(LocalDate.now())
-            .nickNames(b -> b
-                    .add("Test1")
-                    .add("Test2"))
-            .mannschaft(mb -> mb
-                    .name("Testmannschaft")
-                    .sponsoren(sb -> sb
-                            .add(SponsorDtoBuilder
-                                    .create()
-                                    .name("TestSponsor")
-                                    .build()
-                            )
-                    )
-            )
-            .name(this::nameSupplier).build();
+            .nickNames(b -> b.add("Test1").add("Test2"))
+            .mannschaft(
+                mb ->
+                    mb.name("Testmannschaft")
+                        .sponsoren(
+                            sb -> sb.add(SponsorDtoBuilder.create().name("TestSponsor").build())))
+            .name(this::nameSupplier)
+            .build();
     assertNotNull(personDto);
     assertNotNull(personDto.getBirthdate());
     assertEquals("Testname", personDto.getName());
