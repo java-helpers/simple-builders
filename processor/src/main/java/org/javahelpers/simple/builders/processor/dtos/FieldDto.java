37e85dfe68ece89d7ecfc84b3372e542e60facc2
Andreas Igel <193066328+AndreasIgel@users.noreply.github.com>
2025-05-09 21:31:38 +0200

    adding explicit setter property in builder definition

--- processor/src/main/java/org/javahelpers/simple/builders/processor/dtos/FieldDto.java
+++ processor/src/main/java/org/javahelpers/simple/builders/processor/dtos/FieldDto.java
@@ -37,6 +37,9 @@ public class FieldDto {
 
   /** Type of field. Containing generic, name of package and class */
   private TypeName fieldType;
+  
+  /** Name of setter in target object. Entry may be {@code null} if field is only been set via constructor */ 
+  private final String setterName; 
 
   /** List of all methods in builder, which change the field. */
   private final List<MethodDto> fieldSetterMethodsList = new ArrayList<>();
@@ -58,6 +61,14 @@ public class FieldDto {
   public void setFieldName(String fieldName) {
     this.fieldName = fieldName;
   }
+  
+  public Optional<String> getSetterName() {
+    return Optional.ofNullable(setterName);
+  }
+  
+  public void setSetterName(String setterName) {
+    this.setterName = setterName;
+  } 
 
   /**
    * Getting type of field.
